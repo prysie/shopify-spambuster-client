6152df1a1b5187916a956454e67350fcab39a1bb
Chris Hjorth <contact@chrishjorth.com>
2020-05-22 14:13:34 +0200

    Build.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 const sjcl = require('sjcl')
 
-const STAGE = 'dev'
-// const STAGE = 'prod'
-
 const config = {
-  BACKEND_URL: STAGE === 'dev' ? 'https://v7qqtjkwvj.execute-api.eu-west-1.amazonaws.com/dev' : 'https://a8w3q11yde.execute-api.eu-west-1.amazonaws.com/prod'
+  BACKEND_URL: process.env.NODE_ENV === 'production' ? 'https://a8w3q11yde.execute-api.eu-west-1.amazonaws.com/prod' : 'https://v7qqtjkwvj.execute-api.eu-west-1.amazonaws.com/dev',
+  SCRIPTSRC: process.env.NODE_ENV === 'production' ? 'https://www.chrishjorth.com/shopify-spambuster-client/build/spambuster.js' : 'https://www.chrishjorth.com/shopify-spambuster-client/build/spambuster-dev.js'
 }
 
-console.log('Spambuster v0.1.0 - ' + STAGE)
+console.log('Spambuster v0.1.0 - ' + process.env.NODE_ENV)
 
 window.$(function ($) {
-  const SCRIPTSRC = 'https://www.chrishjorth.com/shopify-spambuster-client/build/spambuster.js'
+  const SCRIPTSRC = config.SCRIPTSRC
   const BACKEND_URL = config.BACKEND_URL
   const RECAPTCHA_SCRIPT_SRC = 'https://www.google.com/recaptcha/api.js'
   const RECAPTCHA_TEXT = '' +
