cfd0e3e53306a6124733b845cec6ff2d35f8659f
Chris Hjorth <contact@chrishjorth.com>
2021-03-07 15:24:20 +0100

    Built and bumped dev to 2.2.0, removed jQuery dependency.

--- src/index.js
+++ src/index.js
@@ -5,270 +5,268 @@ const config = {
   SCRIPTSRC: process.env.NODE_ENV === 'production' ? 'spambuster.js' : 'spambuster-dev.js'
 }
 
-console.log('Spambuster v2.1.8 - ' + process.env.NODE_ENV)
+console.log('Spambuster v2.2.0 - ' + process.env.NODE_ENV)
 
-window.$(function ($) {
-  const mnslpPost = (url, data, callback) => {
-    const xhr = new window.XMLHttpRequest()
-    xhr.open('POST', url, true)
-    xhr.setRequestHeader('Content-Type', 'application/json')
+const mnslpPost = (url, data, callback) => {
+  const xhr = new window.XMLHttpRequest()
+  xhr.open('POST', url, true)
+  xhr.setRequestHeader('Content-Type', 'application/json')
 
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === window.XMLHttpRequest.DONE) {
-        if (xhr.status === 200) {
-          callback(null, xhr.responseText)
-        } else {
-          const error = 'mnslpPost request failed.'
-          callback(error)
-        }
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === window.XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        callback(null, xhr.responseText)
+      } else {
+        const error = 'mnslpPost request failed.'
+        callback(error)
       }
     }
-    xhr.send(JSON.stringify(data))
+  }
+  xhr.send(JSON.stringify(data))
+}
+
+const SCRIPTSRC = config.SCRIPTSRC
+const BACKEND_URL = config.BACKEND_URL
+const RECAPTCHA_SCRIPT_SRC = 'https://www.google.com/recaptcha/api.js'
+const RECAPTCHA_TEXT = '' +
+  '<div class="mssb-rc-text">' +
+  'This site is protected by reCAPTCHA and the Google' +
+  ' <a href="https://policies.google.com/privacy" target="_blank">Privacy Policy</a> and' +
+  ' <a href="https://policies.google.com/terms" target="_blank">Terms of Service</a> apply.' +
+  '</div>'
+
+let hasForm = false
+let canSubmitCommentForm = false
+let canSubmitSignupForm = false
+let canSubmitLoginForm = false
+
+const shop = window.Shopify.shop
+
+const scripts = document.getElementsByTagName('script')
+let rcSiteKey = ''
+let contactEnabled = ''
+for (let i = 0; i < scripts.length; i++) {
+  const scriptSrcSegs = scripts[i].src.split('/')
+  const script = scriptSrcSegs[scriptSrcSegs.length - 1]
+  const src = script.substring(0, script.indexOf('?'))
+  if (src === SCRIPTSRC) {
+    rcSiteKey = script.substring((src + '?rcSiteKey=').length)
+    rcSiteKey = rcSiteKey.substring(0, rcSiteKey.indexOf('&'))
+
+    const index = script.indexOf('contactEnabled=')
+    contactEnabled = index > -1 ? script.substring(index + 'contactEnabled='.length, script.indexOf('&', index)) === 'true' : false
+  }
+}
+
+// https://developers.google.com/recaptcha/docs/faq
+// https://github.com/google/google-api-javascript-client/issues/397
+// https://community.shopify.com/c/Technical-Q-A/GTM-on-Shopify-Plus-store-now-Reporting-CSP-issues/m-p/666613
+// Shopify CSP headers are set to report scripts but still allow them to run
+const nonce = 'this_is_my_nonce'
+const scriptNode = document.createElement('script')
+scriptNode.src = RECAPTCHA_SCRIPT_SRC + '?render=' + rcSiteKey
+scriptNode.type = 'text/javascript'
+scriptNode.charset = 'utf-8'
+scriptNode.nonce = nonce
+document.getElementsByTagName('head')[0].appendChild(scriptNode)
+
+const $newCommentForm = document.querySelectorAll('#comment_form')
+const $contactForm = document.querySelectorAll('form.contact-form')
+const $signupForm = document.querySelectorAll('#RegisterForm')
+const $loginForm = document.querySelectorAll('#customer_login')
+
+// We generate the hash locally because we do not want to send user data to our servers.
+// If the same person makes the same comment on the site we have a collision ->
+// result is simply that one risks being marked as spam, which makes sense since it is duplication
+const getHash = function (name, email, body, shop) {
+  const out = sjcl.hash.sha256.hash(name + email + body + shop)
+  const hash = sjcl.codec.hex.fromBits(out)
+  return hash
+}
+
+const commentVerifyReCaptcha = function () {
+  if (!window.grecaptcha) {
+    console.error('Error with Google ReCaptcha on comment form')
+    return
   }
 
-  const SCRIPTSRC = config.SCRIPTSRC
-  const BACKEND_URL = config.BACKEND_URL
-  const RECAPTCHA_SCRIPT_SRC = 'https://www.google.com/recaptcha/api.js'
-  const RECAPTCHA_TEXT = '' +
-    '<div class="mssb-rc-text">' +
-    'This site is protected by reCAPTCHA and the Google' +
-    ' <a href="https://policies.google.com/privacy" target="_blank">Privacy Policy</a> and' +
-    ' <a href="https://policies.google.com/terms" target="_blank">Terms of Service</a> apply.' +
-    '</div>'
+  window.grecaptcha.ready(function () {
+    try {
+      window.grecaptcha.execute(rcSiteKey, { action: 'blog_comment' })
+        .then(function (token) {
+          const commentName = $newCommentForm.querySelectorAll('input[name="comment[author]"]')[0].value
+          const commentEmail = $newCommentForm.querySelectorAll('input[name="comment[email]"]')[0].value
+          const commentBody = $newCommentForm.querySelectorAll('textarea[name="comment[body]"]')[0].value
 
-  let hasForm = false
-  let canSubmitCommentForm = false
-  let canSubmitSignupForm = false
-  let canSubmitLoginForm = false
+          const data = {
+            shop: shop,
+            token: token,
+            commentHash: getHash(commentName, commentEmail, commentBody, shop)
+          }
 
-  const shop = window.Shopify.shop
-
-  const scripts = document.getElementsByTagName('script')
-  let rcSiteKey = ''
-  let contactEnabled = ''
-  for (let i = 0; i < scripts.length; i++) {
-    const scriptSrcSegs = scripts[i].src.split('/')
-    const script = scriptSrcSegs[scriptSrcSegs.length - 1]
-    const src = script.substring(0, script.indexOf('?'))
-    if (src === SCRIPTSRC) {
-      rcSiteKey = script.substring((src + '?rcSiteKey=').length)
-      rcSiteKey = rcSiteKey.substring(0, rcSiteKey.indexOf('&'))
-
-      const index = script.indexOf('contactEnabled=')
-      contactEnabled = index > -1 ? script.substring(index + 'contactEnabled='.length, script.indexOf('&', index)) === 'true' : false
-    }
-  }
-
-  // https://developers.google.com/recaptcha/docs/faq
-  // https://github.com/google/google-api-javascript-client/issues/397
-  // https://community.shopify.com/c/Technical-Q-A/GTM-on-Shopify-Plus-store-now-Reporting-CSP-issues/m-p/666613
-  // Shopify CSP headers are set to report scripts but still allow them to run
-  const nonce = 'this_is_my_nonce'
-  const scriptNode = document.createElement('script')
-  scriptNode.src = RECAPTCHA_SCRIPT_SRC + '?render=' + rcSiteKey
-  scriptNode.type = 'text/javascript'
-  scriptNode.charset = 'utf-8'
-  scriptNode.nonce = nonce
-  document.getElementsByTagName('head')[0].appendChild(scriptNode)
-
-  const $newCommentForm = $('#comment_form')
-  const $contactForm = $('form.contact-form')
-  const $signupForm = $('#RegisterForm')
-  const $loginForm = $('#customer_login')
-
-  // We generate the hash locally because we do not want to send user data to our servers.
-  // If the same person makes the same comment on the site we have a collision ->
-  // result is simply that one risks being marked as spam, which makes sense since it is duplication
-  const getHash = function (name, email, body, shop) {
-    const out = sjcl.hash.sha256.hash(name + email + body + shop)
-    const hash = sjcl.codec.hex.fromBits(out)
-    return hash
-  }
-
-  const commentVerifyReCaptcha = function () {
-    if (!window.grecaptcha) {
-      console.error('Error with Google ReCaptcha on comment form')
-      return
-    }
-
-    window.grecaptcha.ready(function () {
-      try {
-        window.grecaptcha.execute(rcSiteKey, { action: 'blog_comment' })
-          .then(function (token) {
-            const commentName = $('input[name="comment[author]"]', $newCommentForm).val()
-            const commentEmail = $('input[name="comment[email]"]', $newCommentForm).val()
-            const commentBody = $('textarea[name="comment[body]"]', $newCommentForm).val()
-
-            const data = {
-              shop: shop,
-              token: token,
-              commentHash: getHash(commentName, commentEmail, commentBody, shop)
+          mnslpPost(BACKEND_URL + '/verify', data, (error, data) => {
+            if (error !== null) {
+              throw new Error(error)
             }
-
-            mnslpPost(BACKEND_URL + '/verify', data, (error, data) => {
-              if (error !== null) {
-                throw new Error(error)
-              }
-              data = JSON.parse(data)
-              if (parseFloat(data.score) > 0.5) {
-                canSubmitCommentForm = true
-                $newCommentForm.submit()
-              } else {
-                window.alert('The spam protection system did now allow this comment.\nIf this is not spam please verify your internet connection or contact us via email.')
-              }
-            })
+            data = JSON.parse(data)
+            if (parseFloat(data.score) > 0.5) {
+              canSubmitCommentForm = true
+              $newCommentForm.submit()
+            } else {
+              window.alert('The spam protection system did now allow this comment.\nIf this is not spam please verify your internet connection or contact us via email.')
+            }
           })
-      } catch (error) {
-        console.error(error)
-        window.alert('Error posting the comment. Please try again at a later time.')
-      }
-    })
+        })
+    } catch (error) {
+      console.error(error)
+      window.alert('Error posting the comment. Please try again at a later time.')
+    }
+  })
+}
+
+const verifyReCaptcha = function (action, callback) {
+  if (!window.grecaptcha) {
+    console.error('Error with Google ReCaptcha on contact form')
+    return
   }
 
-  const verifyReCaptcha = function (action, callback) {
-    if (!window.grecaptcha) {
-      console.error('Error with Google ReCaptcha on contact form')
-      return
+  window.grecaptcha.ready(function () {
+    try {
+      window.grecaptcha.execute(rcSiteKey, { action: action })
+        .then(function (token) {
+          const data = {
+            shop: shop,
+            token: token
+          }
+
+          mnslpPost(BACKEND_URL + '/verifyonly', data, (error, data) => {
+            if (error !== null) {
+              throw new Error(error)
+            }
+            data = JSON.parse(data)
+            if (parseFloat(data.score) > 0.5) {
+              callback(null)
+            } else {
+              window.alert('The spam protection system did now allow this submission.\nIf this is not spam please verify your internet connection or contact us via email.')
+            }
+          })
+        })
+    } catch (error) {
+      console.error(error)
+      window.alert('Error submitting. Please try again at a later time.')
+    }
+  })
+}
+
+const contactVerifyReCaptcha = function ($verifyForm) {
+  if (!window.grecaptcha) {
+    console.error('Error with Google ReCaptcha on contact form')
+    return
+  }
+
+  window.grecaptcha.ready(function () {
+    try {
+      window.grecaptcha.execute(rcSiteKey, { action: 'contact' })
+        .then(function (token) {
+          const data = {
+            shop: shop,
+            token: token
+          }
+
+          mnslpPost(BACKEND_URL + '/verifyonly', data, (error, data) => {
+            if (error !== null) {
+              throw new Error(error)
+            }
+            data = JSON.parse(data)
+            if (parseFloat(data.score) > 0.5) {
+              $verifyForm.submit()
+            } else {
+              window.alert('The spam protection system did now allow this submission.\nIf this is not spam please verify your internet connection or contact us via email.')
+            }
+          })
+        })
+    } catch (error) {
+      console.error(error)
+      window.alert('Error submitting. Please try again at a later time.')
+    }
+  })
+}
+
+const signupVerifyReCaptcha = function () {
+  verifyReCaptcha('signup', function (error) {
+    if (error !== null) {
+      console.error(error)
+    }
+    canSubmitSignupForm = true
+    $signupForm.submit()
+  })
+}
+
+const loginVerifyReCaptcha = function () {
+  verifyReCaptcha('login', function (error) {
+    if (error !== null) {
+      console.error(error)
+    }
+    canSubmitLoginForm = true
+    $loginForm.submit()
+  })
+}
+
+if ($newCommentForm.length > 0) {
+  hasForm = true
+  $newCommentForm.on('submit', function () {
+    if (canSubmitCommentForm === false) {
+      setTimeout(commentVerifyReCaptcha, 1)
+    }
+    return canSubmitCommentForm
+  })
+
+  $newCommentForm.append(RECAPTCHA_TEXT)
+}
+
+if ($contactForm.length > 0 && contactEnabled === true) {
+  hasForm = true
+
+  $contactForm.on('submit', function (e) {
+    const middleMan = function () {
+      contactVerifyReCaptcha(e.target)
     }
 
-    window.grecaptcha.ready(function () {
-      try {
-        window.grecaptcha.execute(rcSiteKey, { action: action })
-          .then(function (token) {
-            const data = {
-              shop: shop,
-              token: token
-            }
+    setTimeout(middleMan, 1)
 
-            mnslpPost(BACKEND_URL + '/verifyonly', data, (error, data) => {
-              if (error !== null) {
-                throw new Error(error)
-              }
-              data = JSON.parse(data)
-              if (parseFloat(data.score) > 0.5) {
-                callback(null)
-              } else {
-                window.alert('The spam protection system did now allow this submission.\nIf this is not spam please verify your internet connection or contact us via email.')
-              }
-            })
-          })
-      } catch (error) {
-        console.error(error)
-        window.alert('Error submitting. Please try again at a later time.')
-      }
-    })
-  }
+    return false // The submit called from the contactVerifyReCaptcha function does not trigger this handler
+    // This means that the form can only be submitted if it
+  })
 
-  const contactVerifyReCaptcha = function ($verifyForm) {
-    if (!window.grecaptcha) {
-      console.error('Error with Google ReCaptcha on contact form')
-      return
+  $contactForm.append(RECAPTCHA_TEXT)
+}
+
+if ($signupForm.length > 0 && contactEnabled === true) {
+  hasForm = true
+
+  $signupForm.on('submit', function () {
+    if (canSubmitSignupForm === false) {
+      setTimeout(signupVerifyReCaptcha, 1)
     }
+    return canSubmitSignupForm
+  })
 
-    window.grecaptcha.ready(function () {
-      try {
-        window.grecaptcha.execute(rcSiteKey, { action: 'contact' })
-          .then(function (token) {
-            const data = {
-              shop: shop,
-              token: token
-            }
+  $signupForm.append(RECAPTCHA_TEXT)
+}
 
-            mnslpPost(BACKEND_URL + '/verifyonly', data, (error, data) => {
-              if (error !== null) {
-                throw new Error(error)
-              }
-              data = JSON.parse(data)
-              if (parseFloat(data.score) > 0.5) {
-                $verifyForm.submit()
-              } else {
-                window.alert('The spam protection system did now allow this submission.\nIf this is not spam please verify your internet connection or contact us via email.')
-              }
-            })
-          })
-      } catch (error) {
-        console.error(error)
-        window.alert('Error submitting. Please try again at a later time.')
-      }
-    })
-  }
+if ($loginForm.length > 0 && contactEnabled === true) {
+  hasForm = true
 
-  const signupVerifyReCaptcha = function () {
-    verifyReCaptcha('signup', function (error) {
-      if (error !== null) {
-        console.error(error)
-      }
-      canSubmitSignupForm = true
-      $signupForm.submit()
-    })
-  }
+  $loginForm.on('submit', function () {
+    if (canSubmitLoginForm === false) {
+      setTimeout(loginVerifyReCaptcha, 1)
+    }
+    return canSubmitLoginForm
+  })
 
-  const loginVerifyReCaptcha = function () {
-    verifyReCaptcha('login', function (error) {
-      if (error !== null) {
-        console.error(error)
-      }
-      canSubmitLoginForm = true
-      $loginForm.submit()
-    })
-  }
+  $loginForm.append(RECAPTCHA_TEXT)
+}
 
-  if ($newCommentForm.length > 0) {
-    hasForm = true
-    $newCommentForm.on('submit', function () {
-      if (canSubmitCommentForm === false) {
-        setTimeout(commentVerifyReCaptcha, 1)
-      }
-      return canSubmitCommentForm
-    })
-
-    $newCommentForm.append(RECAPTCHA_TEXT)
-  }
-
-  if ($contactForm.length > 0 && contactEnabled === true) {
-    hasForm = true
-
-    $contactForm.on('submit', function (e) {
-      const middleMan = function () {
-        contactVerifyReCaptcha(e.target)
-      }
-      
-      setTimeout(middleMan, 1)
-
-      return false // The submit called from the contactVerifyReCaptcha function does not trigger this handler
-      // This means that the form can only be submitted if it 
-    })
-
-    $contactForm.append(RECAPTCHA_TEXT)
-  }
-
-  if ($signupForm.length > 0 && contactEnabled === true) {
-    hasForm = true
-
-    $signupForm.on('submit', function () {
-      if (canSubmitSignupForm === false) {
-        setTimeout(signupVerifyReCaptcha, 1)
-      }
-      return canSubmitSignupForm
-    })
-
-    $signupForm.append(RECAPTCHA_TEXT)
-  }
-
-  if ($loginForm.length > 0 && contactEnabled === true) {
-    hasForm = true
-
-    $loginForm.on('submit', function () {
-      if (canSubmitLoginForm === false) {
-        setTimeout(loginVerifyReCaptcha, 1)
-      }
-      return canSubmitLoginForm
-    })
-
-    $loginForm.append(RECAPTCHA_TEXT)
-  }
-
-  if (hasForm === true) {
-    document.head.insertAdjacentHTML('beforeend', '<style>.grecaptcha-badge { visibility: hidden; }</style>')
-  }
-})
+if (hasForm === true) {
+  document.head.insertAdjacentHTML('beforeend', '<style>.grecaptcha-badge { visibility: hidden; }</style>')
+}
